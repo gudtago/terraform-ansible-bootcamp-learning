890874675101bd290bd2a6c0dcb4376925421b61
gudtago <1972612+gudtago@users.noreply.github.com>
2024-05-26 16:47:03 -0400

    added requiredPorviders

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -3,11 +3,9 @@
 # Configure the AWS Provider
 provider "aws" {
     region = "us-east-2"
-    version = "5.51.1"
 }
 
 # Configure the Microsoft Asure Provider
 provider "azurerm" {
-  version = "3.105.0"
   features {}
 }
---
+++ Start With Terraform Basics/requiredProviders.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"      
+        version = "~> 5.51.1"  
+    }
+
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+    }
+  }  
+}

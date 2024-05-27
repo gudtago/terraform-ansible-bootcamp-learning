c6eee588efa777378ea41d75003adb5f28d927d7
gudtago <1972612+gudtago@users.noreply.github.com>
2024-05-27 10:49:16 -0400

    updated

--- Start With Terraform Basics/createinstance.tf
+++ Start With Terraform Basics/createinstance.tf
@@ -1,11 +1,8 @@
-provider "aws" {
-    access_key = "AKIAUXKOWANTFYBQ6BNT"
-    secret_key = "SECRET_KEY_HERE"
-    region     = "us-east-2"
-}
-
 resource "aws_instance" "MyFIrstInstance" {
-ami             = "ami-02b65c98a0f57871"
-instance_type   = "t2.micro"
+    ami             = "ami-02b65c98a0f57871"
+    instance_type   = "t2.micro"
+    access_key      = "AKIAUXKOWANTFYBQ6BNT"
+    secret_key      = "SECRET_KEY_HERE"
+    region          = "us-east-2"
 }
 

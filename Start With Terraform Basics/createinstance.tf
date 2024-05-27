7c70ec3134aa327ff735e73856f6c8182c7bc3c6
gudtago <1972612+gudtago@users.noreply.github.com>
2024-05-27 10:51:49 -0400

    deleted

--- Start With Terraform Basics/createinstance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "MyFIrstInstance" {
-    ami             = "ami-02b65c98a0f57871"
-    instance_type   = "t2.micro"
-    access_key      = "AKIAUXKOWANTFYBQ6BNT"
-    secret_key      = "SECRET_KEY_HERE"
-    region          = "us-east-2"
-}
-
